488a529e9aec37baf7721fa1ad8a80ad80c1bc39
seatanaka <tanas007@newschool.edu>
2023-12-05 19:58:48 -0500

    new publish

---
+++ entry 1/script entry 1.js
@@ -0,0 +1,16 @@
+$("document").ready(function() {
+
+    $("h1").click(function() {
+        $("nav").toggle();
+    });
+
+
+    $(document).ready(function(){
+        // Toggle the visibility of an element with ID "myElement" when a button is clicked
+        $("#myButton").click(function(){
+          $("#myElement").toggle();
+        });
+      });
+
+
+});
