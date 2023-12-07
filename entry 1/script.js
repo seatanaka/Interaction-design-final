208372acf66389dc651ac90209c2580d4e3e3083
seatanaka <tanas007@newschool.edu>
2023-12-06 23:26:47 -0500

    update

---
+++ entry 1/script.js
@@ -0,0 +1,15 @@
+$("document").ready(function() {
+
+  $("#languageswitch").click(function(){
+    console.log("working")
+    $(".English").toggle();
+    $(".Japanese").toggle();
+  });
+
+    $("h1").click(function() {
+        
+    });
+
+
+
+});
